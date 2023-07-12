5832bb0d552463467b56d372de3d532077a0defb
odi tamunodiepiriye <89323758+pearlodi@users.noreply.github.com>
2023-07-12 14:19:14 +0100

    Update DisplayPage.js

--- src/components/MovieApp/DisplayPage.js
+++ src/components/MovieApp/DisplayPage.js
@@ -6,7 +6,7 @@ const DisplayPage = () => {
   return (
     <div className='display'>
       <div className='displays'>
-        <p className='disp'>Unlimited movies, TV shows, and more
+        <p className='disp'>Unlimited movies, TV show, and more
 </p>
 <p className='display_2'>Anywhere, Anytime</p>
 <br></br>
