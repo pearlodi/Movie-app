f85af345907932b05a6e65318ec80c68a3ee02fb
pearlodi7@gmail.com <you@example.com>
2023-11-22 15:27:39 +0000

    Fix build issue

--- src/components/MovieApp/DisplayPage.js
+++ src/components/MovieApp/DisplayPage.js
@@ -10,7 +10,7 @@ const DisplayPage = () => {
   <>  <div className='display'>
   <div className='overlay'>
  <div className='display-info'>
- <p className='display-text'>YOUR ONE STOP <span className='span'>MOVIE</span> </p>
+ <p className='display-text'>YOUR ONE STOP <span className='span'>MOVIE</span> PALACE</p>
  <p className='display-title'>Search for unlimited movies from various genres to watch next</p>
 {/* <p className='display_text-b'>Anywhere, Anytime</p> */}
 <Link to="/elementList" className='display_3'>Get started<img src='arrow.svg' alt='' /></Link>
