22137e816dd81d7c6526765a30a46ebf95b7d3a6
pearlodi7@gmail.com <you@example.com>
2023-11-22 15:24:45 +0000

    Fix build issue

--- src/components/MovieApp/CemedyList.js
+++ src/components/MovieApp/CemedyList.js
@@ -1,16 +1,12 @@
 import axios from 'axios';
-import './MovieApp.css'
+import './MovieApp.css';
 import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
+
 const CemedyList = () => {
   const [movies, setMovies] = useState([]);
-  const [selectedGenre, setSelectedGenre] = useState('comedy');
+  const selectedGenre = useState('comedy')[0]; // Accessing the state variable directly
 
-
-  useEffect(() => {
-    fetchMovies();
-  }, [selectedGenre]);
-  
   const fetchMovies = async () => {
     try {
       const response = await axios.get('https://yts.mx/api/v2/list_movies.json', {
@@ -19,34 +15,36 @@ const CemedyList = () => {
         },
       });
       const data = response.data.data.movies;
-      setMovies(data.slice(0, 10)); // Displaying the first 10 movies of the selected genre
+      setMovies(data.slice(0, 10));
     } catch (error) {
       console.log(error);
     }
   };
-  
-  
+
+  useEffect(() => {
+    fetchMovies();
+  },);
 
   return (
     <div className='movie_app'>
-
-<p className='movie-genre'>COMEDY</p>
-<div className='line'></div>
-<div className='select_details'>
-
-{movies.map((movie) => (
-
-<div className='movie-card'>
-<h3 className='movie_title'>{movie.title.length > 20 ? movie.title.substr(0, 10) : movie.title}</h3>
-   <div className='movie-img'>
-   <img src={movie.large_cover_image} className='movie--img' alt='movie cover'/>
-  </div>
-  <Link to={`/details/${movie.id}`} className='movie_link'>View Details</Link>
-</div>
-  
-))}
-</div>
-</div>
+      <p className='movie-genre'>COMEDY</p>
+      <div className='line'></div>
+      <div className='select_details'>
+        {movies.map((movie) => (
+          <div className='movie-card' key={movie.id}>
+            <h3 className='movie_title'>
+              {movie.title.length > 20 ? movie.title.substr(0, 10) : movie.title}
+            </h3>
+            <div className='movie-img'>
+              <img src={movie.large_cover_image} className='movie--img' alt='movie cover' />
+            </div>
+            <Link to={`/details/${movie.id}`} className='movie_link'>
+              View Details
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
--- src/components/MovieApp/FantasyList.js
+++ src/components/MovieApp/FantasyList.js
@@ -4,13 +4,10 @@ import { Link } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 const HorrorsList = () => {
   const [movies, setMovies] = useState([]);
-  const [selectedGenre, setSelectedGenre] = useState('horror');
+  const selectedGenre = useState('horror')[0];
 
 
-  useEffect(() => {
-    fetchMovies();
-  }, [selectedGenre]);
-  
+ 
   const fetchMovies = async () => {
     try {
       const response = await axios.get('https://yts.mx/api/v2/list_movies.json', {
@@ -25,6 +22,9 @@ const HorrorsList = () => {
     }
   };
   
+  useEffect(() => {
+    fetchMovies();
+  },);
   
 
   return (
